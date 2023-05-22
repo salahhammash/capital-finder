d4a049604046ec31c261eb5e2e40f80a411facbe
SalahHammash <s.m.hammash@gmail.com>
2023-05-22 10:36:44 +0300

    done

--- api/capitalfinder.py
+++ api/capitalfinder.py
@@ -21,22 +21,8 @@ class handler(BaseHTTPRequestHandler):
             msg = f'The capital of {country} is {req}'
     
     
-    # def do_GET(self):
-    #     s = self.path
-    #     url_components = parse.urlsplit(s)
-    #     query_strings_list = parse.parse_qsl(url_components.query)
-    #     dic = dict(query_strings_list)
-    #     country = dic.get("country")
-    #     capital = dic.get("capital")
-
-
-    #     if country:
-    #         url = f"https://restcountries.com/v3.1/name/%7Bcountry%7D"
-    #         res = requests.get(url)
-    #         data = res.json()
-    #         result = data[0]["capital"][0]
-    #         msg = f'The capital of {country} is {result}'
-         
+                #  
+        
         elif capital :
             url =f'https://restcountries.com/v3.1/capital/{capital}'
             res = requests.get(url)
